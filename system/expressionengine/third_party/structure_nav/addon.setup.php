03e20143c26610e4f6c72af2f3133f4c71bfb248
Brian Litzinger <litzinger@gmail.com>
2017-05-15 11:06:13 -0500

    Add an addon.setup.php file for EE 3 compatibility

---
+++ system/expressionengine/third_party/structure_nav/addon.setup.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'author'        => 'Rob Sanchez',
+    'author_url'    => '',
+    'docs_url'      => '',
+    'name'          => 'Structure Nav',
+    'description'   => '',
+    'version'       => '1.0.7',
+    'namespace'     => 'StructureNav',
+    'settings_exist' => false,
+];
